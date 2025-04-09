ecd67de5dbd087cb16b8b2e0e91a61c142bef716
Juan Luis Hernández Terrón <72230108+JuanLuisHernandezTerron@users.noreply.github.com>
2025-04-09 21:45:40 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,10 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
+    print(config_name)
     app.config.from_object(config_dict[config_name])
 
     # Initialize the database
-    print(config_name)
     db.init_app(app)
 
     # Import blueprints/routes
