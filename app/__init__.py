646134fdd9e90ae4172b0ba4d0f74df361ba54e5
Juan Luis Hernández Terrón <72230108+JuanLuisHernandezTerron@users.noreply.github.com>
2025-04-09 22:52:03 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,9 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
     print(f" este es el config name {config_name}", flush=True)
+    
     app.config.from_object(config_dict[config_name])
-
+    print(f" este es el config dict {config_dict}", flush=True)
     # Initialize the database
     db.init_app(app)
 
