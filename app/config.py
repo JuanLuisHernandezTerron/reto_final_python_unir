bf11d9a2cef9f468bb7dee010982cb91be4b5f64
Juan Luis Hernández Terrón <72230108+JuanLuisHernandezTerron@users.noreply.github.com>
2025-04-09 22:10:38 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,7 +6,8 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key")
 
     # Database configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_UR = 'postgresql://postgres:postgres@db:5432/mydatabase'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Add other configuration variables as needed
