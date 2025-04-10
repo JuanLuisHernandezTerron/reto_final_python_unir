10f49552e43ea104d86b8bc602e65695c6abe0c8
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2025-04-10 17:39:11 +0200

    feat: add

--- app/config.py
+++ app/config.py
@@ -4,7 +4,6 @@ import os
 class Config:
     # Secret key for the Flask app
     SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key")
-
     # Database configuration
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     print(SQLALCHEMY_DATABASE_URI)
@@ -21,10 +20,13 @@ class ProductionConfig(Config):
     DEBUG = False
     # Add other production configurations here
 
+class TestingConfig(Config):
+    TESTING = True
 
 # Dictionary to map environment names to configuration classes
 config_dict = {
     "development": DevelopmentConfig,
     "production": ProductionConfig,
+    "testing":TestingConfig
     # Add other environments if needed
 }
