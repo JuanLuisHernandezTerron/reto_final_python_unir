1c38d360da3b9634750a9bd6e85bcd8459a73c43
Juan Luis Hernández Terrón <72230108+JuanLuisHernandezTerron@users.noreply.github.com>
2025-04-10 17:49:20 +0200

    Update test_routes.py

--- app/test_routes.py
+++ app/test_routes.py
@@ -1,7 +1,22 @@
 from flask import request
+import pytest
+from app import create_app, db
+@pytest.fixture
+def client():
+    app = create_app("testing")
+    app.config['TESTING'] = True
+    
+    with app.app_context():
+        db.create_all()
+
+    with app.test_client() as client:
+        yield client
+
+    with app.app_context():
+        db.drop_all()
+        
 def test_insert_data():
     payload = request.get_json()
     response = client.post("/data", json=payload)
     assert response.status_code == 201
     assert response.json["message"] == "Data created"
-
